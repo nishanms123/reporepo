976fe64a00b0ab011376aab286c96366b493e74a
nishan123 <msnishan66@gmail.com>
2023-08-17 15:33:06 +0530

    updated

--- src/test/java/sms1/Eshan.java
+++ src/test/java/sms1/Eshan.java
@@ -3,5 +3,7 @@ package sms1;
 public class Eshan {
    public static void main(String[] args) {
 	System.out.println("----hi----");
+	System.out.println("---blue----");
+	System.out.println("------dark blue-----");
 }
 }
